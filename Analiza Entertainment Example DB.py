175531d7fd4d2184053be4f3de8a83cfd914c2f6
Jakub <jakubmaliglowka@outlook.com>
2023-10-25 18:36:41 +0200

    Zmiany

--- Analiza Entertainment Example DB.py
+++ Analiza Entertainment Example DB.py
@@ -12,6 +12,8 @@ conn = pyodbc.connect(
 )
 
 #Nazwy Tabel postaci tabeli
+
+print("Nazwy tabel w bazie danych")
 query = "SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE'"
 my_cursor = conn.cursor()
 
@@ -19,8 +21,19 @@ my_cursor.execute(query)
 
 table_names = [row.table_name for row in my_cursor.fetchall()]
 
-conn.close()
-
 Data_Frame_Nazwy_Tabel = pd.DataFrame(table_names, columns=["Nazwa Tabeli"])
 
 print(Data_Frame_Nazwy_Tabel)
+
+my_cursor.fetchall()
+
+# Tabela Agents
+query1 = "SELECT * FROM Agents"
+
+my_cursor1 = conn.cursor()
+
+my_cursor1.execute(query1)
+
+for x in my_cursor1:
+    print(x)
+
